c62162530b9354630f0734cf1bb5e1f9b12d6bdd
Tarik B Deliktas <tarikdeliktas@gmail.com>
2023-09-27 00:07:07 -0700

    cross-browser testing, custom helpers w/screenshots

---
+++ helpers.ts
@@ -0,0 +1,8 @@
+export async function loadHomepage(page) {
+    await page.goto('https://www.example.com')
+  }
+  
+  export async function assertTitle(page) {
+    await page.waitForSelector('h1')
+  }
+  
