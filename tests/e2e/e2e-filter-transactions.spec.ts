ff41308bdad88f21d71120b0e6da10d585d98b18
Tarik B Deliktas <tarikdeliktas@gmail.com>
2023-09-28 02:13:11 -0700

    e2e filter transactions by account activity

---
+++ tests/e2e/e2e-filter-transactions.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test'
+
+test.describe("Filter Transactions from Account Activity", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://zero.webappsecurity.com/index.html')
+        await page.click("#signin_button")
+        await page.type("#user_login", "username")
+        await page.type("#user_password",  "password")
+
+        // SSL Certificate error fix by directing url to tranfer-funds after login
+        await page.goto('http://zero.webappsecurity.com/bank/account-activity.html')
+    })
+
+    test("Verify the results for each account", async({ page }) => {
+        await page.click("#account_activity_tab")
+
+        // Select option for Checking account
+        await page.selectOption("#aa_accountId", "2")
+
+        // Verify the number of data in checking account table
+        const checkingAccount = await page.locator(
+            "all_transactions_for_account tbody tr"
+        )
+        await expect(checkingAccount).toHaveCount(3) // 3 record in table
+
+        // Select option for Loan
+        await page.selectOption("#aa_accountId", "3")
+        // Verify the number of data in checking account table
+        const loanAccount = await page.locator(
+            "all_transactions_for_account tbody tr"
+        )
+        await expect(loanAccount).toHaveCount(2)  // 2 record in table
+
+        // Select option for Brokerage
+        await page.selectOption("#aa_accountId", "6")
+        const noResult = await page.locator(".well")
+        await expect(noResult).toBeVisible()
+    })
+})
