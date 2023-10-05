e612de05523697b59412f29c492e2eaddba30c19
Tarik B Deliktas <tarikdeliktas@gmail.com>
2023-10-05 15:17:08 -0700

    POST request api tests for Login flows

--- tests/api/post-api.spec.ts
+++ tests/api/post-api.spec.ts
@@ -14,4 +14,27 @@ test.describe.parallel("API Testing", () => {
         expect(responseBody.id).toBe(343)
         expect(responseBody.createdAt).toBeTruthy()
     })
+
+    test('POST Request - Login', async ({ request }) => {
+        const response = await request.post(`${baseURL}/login`, {
+            data: {
+                email: "eve.holt@reqres.in",
+                password: 'cityslicka'
+            },
+        })
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(200)
+        expect(responseBody.token).toBeTruthy()
+    })
+
+    test('POST Request - Login Failed', async ({ request }) => {
+        const response = await request.post(`${baseURL}/login`, {
+            data: {
+                email: "peter@klaven"
+            },
+        })
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(400)
+        expect(responseBody.error).toBe("Missing password")
+    })
 })
