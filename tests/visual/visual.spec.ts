873fc98eccd3fa9702b0b0682ab55d0713618728
Tarik B Deliktas <tarikdeliktas@gmail.com>
2023-10-05 12:24:46 -0700

    single element snapshot test added for visual regression

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -5,4 +5,10 @@ test.describe('Visual Regression Testing Example', () => {
         await page.goto('https://www.example.com')
         expect (await page.screenshot()).toMatchSnapshot('homepage.png')
     })
+
+    test('Single Element Snapshot', async ({ page }) => {
+        await page.goto('https://www.example.com')
+        const singleElement = await page.$('h1')
+        expect(await singleElement.screenshot()).toMatchSnapshot('page-title.png')
+    })
 })
