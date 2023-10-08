49e285902252ae14d4428a872e02c8c9d68e38c1
Tarik B Deliktas <tarikdeliktas@gmail.com>
2023-10-07 19:39:11 -0700

    created device emulation tests for iOS

---
+++ tests/tips/device-emulator.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect, devices } from '@playwright/test';
+
+test.use({
+  ...devices['iPhone 11'],
+});
+
+test('test', async ({ page }) => {
+  await page.getByRole('complementary').getByRole('button', { name: 'Maybe later' }).click();
+  await page.getByRole('link', { name: 'Harry Jackson', exact: true }).click();
+  await page.getByRole('button', { name: 'Edit' }).click();
+  await page.locator('a').filter({ hasText: 'Edit without logging in' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.getByRole('listitem', { name: 'Caroline Hill' }).locator('a').first().click();
+  await page.getByRole('button', { name: 'Watch' }).click();
+});
+
+/* Note: can run them on terminal by 
+    npx playwright open --device "iPhone 11" wikipedia.org */
