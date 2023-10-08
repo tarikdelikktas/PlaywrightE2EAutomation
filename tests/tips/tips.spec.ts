13cd2b5f86268b6eb819adc4655cf89adacddd11
Tarik B Deliktas <tarikdeliktas@gmail.com>
2023-10-07 19:27:05 -0700

    Mouse Movement Simulation

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -17,10 +17,18 @@ test.describe.only("Tips & Tricks", () => {
 
     const people = ['Mike', 'Judy', 'Peter', 'Elon', 'Alice']
     for(const name of people) {
-        test.only(`Running test for ${name}`, async ({ page }) => {
+        test(`Running test for ${name}`, async ({ page }) => {
             await page.goto('http://zero.webappsecurity.com/index.html')
             await page.type("#searchTerm", `${name}`)
             await page.waitForTimeout(3000)
         })
     }
+
+    test("Mouse Movement Simulation", async ({ page }) => {
+        await page.goto('https://www.example.com')
+        await page.mouse.move(0, 0)
+        await page.mouse.down()
+        await page.mouse.move(0, 100)
+        await page.mouse.up()
+    })
 })
